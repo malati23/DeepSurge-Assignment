4d6b34c6a4880032ec675e17248a40549f60f34d
malati23 <bhuyanmalati476@gmail.com>
2025-09-11 21:30:54 +0530

    Added React project for Assignment-1

---
+++ assignment-2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StudentManagement from "./components/StudentManagement";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <StudentManagement />
+    </div>
+  );
+}
+
+export default App;
